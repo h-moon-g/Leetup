b053ef0853f19e984d2c434e327c341987c11089
Hayden Gogan <h.gogan03@icloud.com>
2023-08-31 10:18:09 -0700

    Fixed event images

--- backend/db/seeders/20230831151327-demo-event-image.js
+++ backend/db/seeders/20230831151327-demo-event-image.js
@@ -12,17 +12,17 @@ module.exports = {
     await EventImage.bulkCreate(
       [
         {
-          groupId: 1,
+          eventId: 1,
           url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fimages%2Fanimals%2Fcat&psig=AOvVaw0ZLTg1nn1JXgSbdEOfcca1&ust=1693956572607000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCOiI8ZCOkoEDFQAAAAAdAAAAABAV",
           preview: true,
         },
         {
-          groupId: 2,
+          eventId: 2,
           url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fdomestic-cat&psig=AOvVaw0ZLTg1nn1JXgSbdEOfcca1&ust=1693956572607000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCOiI8ZCOkoEDFQAAAAAdAAAAABAa",
           preview: true,
         },
         {
-          groupId: 3,
+          eventId: 3,
           url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fblack-cat&psig=AOvVaw0ZLTg1nn1JXgSbdEOfcca1&ust=1693956572607000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCOiI8ZCOkoEDFQAAAAAdAAAAABAg",
           preview: true,
         },
@@ -37,7 +37,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       options,
       {
-        groupId: { [Op.in]: [1, 2, 3] },
+        eventId: { [Op.in]: [1, 2, 3] },
       },
       {}
     );
