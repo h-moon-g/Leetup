f7d57fad12728ecd650136ea8639fb7f22433e31
Hayden Gogan <h.gogan03@icloud.com>
2023-09-02 16:14:41 -0700

    Edit an event

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -13,6 +13,36 @@ const { requireAuth } = require("../../utils/auth.js");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
+const validateEvent = [
+  check("venueId").custom(async (venueId) => {
+    const existingVenue = await Venue.findByPk(venueId);
+    if (!existingVenue) {
+      throw new Error("Venue does not exist");
+    }
+  }),
+  check("name")
+    .isLength({ min: 5 })
+    .withMessage("Name must be at least 5 characters"),
+  check("type")
+    .isIn(["Online", "In person"])
+    .withMessage("Type must be 'Online' or 'In person'"),
+  check("capacity").isInt().withMessage("Capacity must be an integer"),
+  check("price").isInt().withMessage("Price is invalid"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("startDate")
+    .isAfter(Date(Date.now()))
+    .withMessage("Start date must be in the future"),
+  check("endDate").custom(async (endDate, { req }) => {
+    let startDate = req.body.startDate;
+    if (endDate < startDate) {
+      throw new Error("End date is less than start date");
+    }
+  }),
+  handleValidationErrors,
+];
+
 const router = express.Router();
 
 //Get all events
@@ -205,6 +235,86 @@ router.post("/:eventId/images", requireAuth, async (req, res) => {
       id: newEventImage.id,
       url: newEventImage.url,
       preview: newEventImage.preview,
+    });
+  }
+});
+
+//Edit an event
+router.put("/:eventId", requireAuth, validateEvent, async (req, res) => {
+  const event = await Event.findByPk(req.params.eventId);
+  console.log(event);
+  if (!event) {
+    res.status(404);
+    return res.json({
+      message: "Event couldn't be found",
+    });
+  }
+  const group = await Group.findByPk(event.groupId);
+  const organizerId = group.organizerId;
+  const user = await User.findByPk(req.user.id);
+  const membership = await Membership.findOne({
+    where: {
+      groupId: group.id,
+      userId: user.id,
+    },
+  });
+  console.log(membership);
+  if (membership) {
+    if (user.id !== organizerId && membership.status !== "Co-host") {
+      res.status(403);
+      return res.json({
+        message: "Forbidden",
+      });
+    }
+  }
+  if (!membership) {
+    if (user.id !== organizerId) {
+      res.status(403);
+      return res.json({
+        message: "Forbidden",
+      });
+    }
+  } else {
+    const {
+      venueId,
+      name,
+      type,
+      capacity,
+      price,
+      description,
+      startDate,
+      endDate,
+    } = req.body;
+    const venue = await Venue.findByPk(venueId);
+    if (!venue) {
+      res.status(404);
+      return res.json({
+        message: "Venue couldn't be found",
+      });
+    }
+    await event.update({
+      venueId,
+      groupId: group.id,
+      name,
+      type,
+      capacity,
+      price,
+      description,
+      startDate,
+      endDate,
+      updatedAt: Date.now(),
+    });
+    return res.json({
+      id: event.id,
+      groupId: event.groupId,
+      venueId: event.venueId,
+      name: event.name,
+      type: event.type,
+      capacity: event.capacity,
+      price: event.price,
+      description: event.description,
+      startDate: event.startDate,
+      endDate: event.endDate,
     });
   }
 });
