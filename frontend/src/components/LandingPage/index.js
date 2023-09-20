3e13d48119c940115e83e98e6aac54acc12530b8
Hayden Gogan <h.gogan03@icloud.com>
2023-09-19 17:26:26 -0700

    Added landing page html

---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <>
+      <div>
+        <h1 class="lp-h1">The people platform-</h1>
+        <h1 class="lp-h1">Where interests</h1>
+        <h1 class="lp-h1">become friendships</h1>
+        <p class="lp-p-filler">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src="leetup_img_1.jpg" alt="leetup Img 1"></img>
+      </div>
+      <div>
+        <h3 class="lp-h3">How Leetup Works</h3>
+        <p class="lp-p-filler">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div>
+        <img src="leetup_img_2.jpg" alt="leetup Img 2"></img>
+        <a href="">See all groups</a>
+        <p class="lp-p-filler">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <img src="leetup_img_3.jpg" alt="leetup Img 3"></img>
+        <a href="">Find an event</a>
+        <p class="lp-p-filler">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <img src="leetup_img_4.jpg" alt="leetup Img 4"></img>
+        <a href="">Start a new group</a>
+        <p class="lp-p-filler">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <button>Join Leetup</button>
+    </>
+  );
+}
+
+export default LandingPage;
